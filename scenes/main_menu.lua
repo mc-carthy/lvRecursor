f9a3cc986523162c77365ca4369112b3b2d7be06
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-12-04 22:29:13 +0000

    Multiple buttons functioning, called either by reference or label value

--- scenes/main_menu.lua
+++ scenes/main_menu.lua
@@ -7,7 +7,8 @@ function MM:new(scene_manager)
     self.super(scene_manager)
     local sw = love.graphics.getWidth()
     local sh = love.graphics.getHeight()
-    self.button = Button(sw / 2, sh / 2 - 60, 140, 40, "Start!")
+    self.button = Button(sw / 2, sh / 2 - 30, 140, 40, "Start!")
+    self.exit_button = Button(sw / 2, sh / 2 + 30, 140, 40, "Quit!")
     
     self.click = function(btn) self:on_click(btn) end
 end
@@ -22,7 +23,12 @@ end
 
 function MM:on_click(button)
     print(button.label)
-    self.scene_manager:switch("test")
+    if button.label == "Start!" then
+    -- if button == self.button then
+        self.scene_manager:switch("test")
+    elseif button == self.exit_button then
+        love.event.quit()
+    end
 end
 
 function MM:update(dt)
@@ -32,10 +38,12 @@ function MM:update(dt)
         self.button:enabled(not self.button.interactable)
     end
     self.button:update(dt)
+    self.exit_button:update(dt)
 end
 
 function MM:draw()
     self.button:draw()
+    self.exit_button:draw()
     love.graphics.printf("Hello from main_menu!", 0, 25, love.graphics.getWidth(), "center")
 end
 
