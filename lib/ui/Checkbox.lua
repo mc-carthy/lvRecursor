1ee14d8864faf0c0cc3264da31ea8c06c674e0ad
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-12-07 22:12:28 +0000

    Checbox functioning

---
+++ lib/ui/Checkbox.lua
@@ -0,0 +1,122 @@
+local Class = require("lib.Class")
+local Vector2 = require("lib.Vector2")
+local U = require("lib.Utils")
+
+local Checkbox = Class:derive("Checkbox")
+
+local padding = 10
+local box_height_percentage = 0.9
+local border_radius = 5
+local line_width = 5
+
+local function mouse_in_bounds(self, mx ,my) 
+    return mx >= self.pos.x - self.width / 2 and mx <= self.pos.x + self.width / 2 and
+           my >= self.pos.y - self.height / 2 and my <= self.pos.y + self.height / 2
+end
+
+function Checkbox:new(x, y, width, height, text) 
+    self.pos = Vector2(x or 0, y or 0)
+    self.width = width
+    self.height = height
+    self.text = text
+    self.cb_height = self.height * box_height_percentage
+    self.cb_width = self.cb_height
+    self.checked = false
+    
+    -- Checkbox colours
+    self.normal = U.grey(128)
+    self.highlight = U.grey(191)
+    self.pressed = U.grey(255)
+    self.disabled = U.grey(127, 127)
+
+    -- Text colours
+    self.text_normal = U.grey(255)
+    self.text_disabled = U.grey(191)
+    
+    self.colour = self.normal
+    self.text_colour = self.text_normal
+    self.previous_left_click = false
+    self.interactable = true
+end
+
+function Checkbox:update(dt) 
+    if not self.interactable then return end
+    
+        -- TODO Consider caching the mouse position elsewhere
+        local x, y = love.mouse.getPosition()
+        local left_click = love.mouse.isDown(1)
+        local in_bounds = U.mouse_in_rect(x, y, self.pos.x, self.pos.y, self.width,  self.height)
+    
+        if in_bounds and not left_click then
+            if self.previous_left_click and self.colour == self.pressed then
+                self.checked = not self.checked
+                _G.events:invoke("onCheckboxClicked", self, self.checked)
+            end
+            self.colour = self.highlight
+        elseif in_bounds and left_click and not self.previous_left_click then
+            self.colour = self.pressed
+        elseif not in_bounds then
+            self.colour = self.normal
+        end
+    
+        self.previous_left_click = left_click
+end
+
+function Checkbox:draw() 
+    -- love.graphics.line(self.pos.x, self.pos.y - self.height / 2, self.pos.x, self.pos.y + self.height / 2)
+    -- love.graphics.line(self.pos.x - self.width / 2, self.pos.y, self.pos.x  + self.width / 2, self.pos.y)
+
+    local r, g, b, a = love.graphics.getColor()
+    local lw = love.graphics.getLineWidth()
+    love.graphics.setColor(self.colour)
+    -- love.graphics.rectangle("line", self.pos.x, self.pos.y, self.width, self.height)
+    love.graphics.setLineWidth(line_width)
+    love.graphics.rectangle("line", self.pos.x, self.pos.y, self.cb_width, self.cb_height, border_radius, border_radius)
+    
+    if self.checked then
+        love.graphics.rectangle("fill", self.pos.x + line_width, self.pos.y + line_width, self.cb_width - line_width * 2, self.cb_height - line_width * 2)
+    end
+
+    love.graphics.setColor(r, g, b, a)
+
+    -- TODO - Consider caching this value
+    local f = love.graphics.getFont()
+    local fw = f:getWidth(self.text)
+    local _, lines = f:getWrap(self.text, self.width - self.cb_width)
+    local fh = f:getHeight();
+
+    love.graphics.setColor(self.text_colour)
+    love.graphics.printf(self.text, self.pos.x + self.cb_width + padding, self.pos.y + self.height / 2 - (fh / 2 * #lines), self.width - self.cb_width - padding, "left")
+    love.graphics.setColor(r, g, b, a)
+    love.graphics.setLineWidth(lw)
+end
+
+function Checkbox:set_box(width, height)
+    height = height or width
+    self.cb_width = math.min(width, self.width)
+    self.cb_height = math.min(height, self.height)
+end
+
+function Checkbox:enabled(enabled)
+    self.interactable = enabled
+    if not enabled then
+        self.colour = self.disabled
+        self.text_colour = self.text_disabled
+    else
+        self.text_colour = self.text_normal
+    end
+end
+
+function Checkbox:colours(normal, highlight, pressed, disabled)
+    assert(type(normal) == "table", "Normal must be a table.")
+    assert(type(highlight) == "table", "Highlight must be a table.")
+    assert(type(pressed) == "table", "Pressed must be a table.")
+    assert(type(disabled) == "table", "Disabled must be a table.")
+
+    self.normal = normal
+    self.highlight = highlight
+    self.pressed = pressed
+    self.disabled = disabled
+end
+
+return Checkbox
--- scenes/main_menu.lua
+++ scenes/main_menu.lua
@@ -4,6 +4,7 @@ local Button = require("lib.ui.Button")
 local Label = require("lib.ui.Label")
 local TextField = require("lib.ui.TextField")
 local Slider = require("lib.ui.Slider")
+local Checkbox = require("lib.ui.Checkbox")
 
 local MM = Scene:derive("main_menu")
 
@@ -25,6 +26,7 @@ function MM:new(scene_manager)
     self.v_slider = Slider(20, 40, 40, 200, "vertical_slider", true)
     self.h_slider_label = Label(sw / 2 + 110, 270, 40, 40, "0", U.grey(255), "left")
     self.v_slider_label = Label(5, 255, 40, 40, "0", U.grey(255), "center")
+    self.cb = Checkbox(love.graphics.getWidth() / 2 - 100, 300, 200, 40, "Check me!")
 
     self.em:add(start_button)
     self.em:add(exit_button)
@@ -34,6 +36,7 @@ function MM:new(scene_manager)
     self.em:add(self.v_slider)
     self.em:add(self.h_slider_label)
     self.em:add(self.v_slider_label)
+    self.em:add(self.cb)
 
     self.click = function(btn) 
         self:on_click(btn) 
@@ -41,6 +44,10 @@ function MM:new(scene_manager)
 
     self.slider_changed = function(slider) 
         self:on_slider_changed(slider)
+    end
+
+    self.checkbox_changed = function(checkbox, value)
+        self:on_checkbox_changed(checkbox, value)
     end
 end
 
@@ -54,12 +61,14 @@ function MM:enter()
     
     _G.events:hook("onBtnClick", self.click)    
     _G.events:hook("onSliderChanged", self.slider_changed)    
+    _G.events:hook("onCheckboxClicked", self.checkbox_changed)    
 end
 
 function MM:exit()
     MM.super.exit(self)
     _G.events:unhook("onBtnClick", self.click)
     _G.events:unhook("onSliderChanged", self.slider_changed)
+    _G.events:unhook("onCheckboxClicked", self.checkbox_changed)
 end
 
 function MM:on_click(button)
@@ -81,6 +90,10 @@ function MM:on_slider_changed(slider)
         self.v_slider_label.text = slider:get_value()
     end
     -- print("Slider " .. slider.id .. " has a value of " .. slider:get_value())
+end
+
+function MM:on_checkbox_changed(checkbox, value)
+    print(checkbox.text .. ": " .. tostring(value))
 end
 
 local prev_down = false
