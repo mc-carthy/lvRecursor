72f4bee274f272960a75c6a639ecc39f69a11afc
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-12-09 14:43:50 +0000

    Implement basic Player movement

--- Player.lua
+++ Player.lua
@@ -15,6 +15,8 @@ local jump = Anim(16, 48, 16, 16, { 1 }, 6, 10)
 local swim = Anim(16, 64, 16, 16, 6, 6, 12)
 local punch = Anim(16, 80, 16, 16, 3, 3, 20, false)
 
+local move_speed = 100
+
 
 function P:new(state)
     if hero_atlas == nil then
@@ -34,6 +36,7 @@ function P:new(state)
         punch = punch
     })
     self.spr:animate("idle")
+    self.vx = 0
 
     self.super.new(self, state)
 end
@@ -41,6 +44,8 @@ end
 function P:update(dt)
     self.super.update(self, dt)
     self.spr:update(dt)
+
+    self.spr.pos.x = self.spr.pos.x + self.vx * move_speed * dt
 end
 
 function P:draw()
@@ -52,7 +57,9 @@ function P:idle_enter(dt)
 end
     
 function P:idle(dt)
-    if Key:key_down("space") then
+    if Key:key("left") or Key:key("right") then
+        self:change("walk")
+    elseif Key:key_down("space") then
         self:change("punch")
     end
 end
@@ -65,6 +72,29 @@ function P:punch_enter(dt)
     self.spr:animate("punch")
     love.audio.stop(snd)        
     love.audio.play(snd)
+end
+
+function P:walk_enter(dt)
+    self.spr:animate("walk")
+end
+
+function P:walk(dt)
+    if Key:key("left") and not Key:key("right") and vx ~= -1 then
+        self.spr:flip_h(true)
+        self.vx = -1
+    elseif Key:key("right") and not Key:key("left") and vx ~= 1 then
+        self.spr:flip_h()
+        self.vx = 1
+    elseif not Key:key("left") and not Key:key("right")then
+        self.vx = 0
+        self:change("idle")
+    end
+    if Key:key_down("space") then
+        self.vx = 0
+        self:change("punch")
+    -- elseif Key:key_down("up") then
+    --     self:change("jump")
+    end
 end
 
 function P:punch(dt)
