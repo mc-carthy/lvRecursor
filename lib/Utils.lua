9534aff265f1ba2d38b43789d1a245fc75fb5f7e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-12-11 20:18:18 +0000

    Circle to circle collision working

--- lib/Utils.lua
+++ lib/Utils.lua
@@ -1,3 +1,6 @@
+local pow = math.pow
+local sqrt = math.sqrt
+
 local U = {}
 
 function U.colour(r, g, b, a)
@@ -22,7 +25,7 @@ function U.mouse_in_rect(mx, my, rx, ry, rw, rh)
     return mx >= rx and mx <= rx + rw and my >= ry and my <= ry + rh
 end
 
-function U.AABBCol(rect1, rect2)
+function U.AABB_col(rect1, rect2)
     local rect1r = rect1.x + rect1.w
     local rect1b = rect1.y + rect1.h
     local rect2r = rect2.x + rect2.w
@@ -30,6 +33,12 @@ function U.AABBCol(rect1, rect2)
 
     return rect1r >= rect2.x and rect2r >= rect1.x and
            rect1b >= rect2.y and rect2b >= rect1.y
+end
+
+function U.circle_to_circle_col(circle1, circle2)
+    local dst = sqrt(pow(circle1.x - circle2.x, 2) + pow(circle1.y - circle2.y, 2))
+
+    return dst < (circle1.r + circle2.r)
 end
 
 return U
--- scenes/test.lua
+++ scenes/test.lua
@@ -12,6 +12,9 @@ function T:new(scene_manager)
     self.e.spr.pos.x = 300
     self.e.spr.tintColour = U.colour(191, 0, 0)
     
+    self.c1 = { x = 200, y = 200, r = 20, c = U.colour(255)}
+    self.c2 = { x = 300, y = 200, r = 40, c = U.colour(127)}
+
     self.em:add(self.p)
     self.em:add(self.e)
 end
@@ -23,10 +26,28 @@ function T:update(dt)
         love.event.quit()
     end
 
-    if U.AABBCol(self.p.spr:rect(), self.e.spr:rect()) then
+    if U.AABB_col(self.p.spr:rect(), self.e.spr:rect()) then
         self.p.spr.tintColour = U.colour(0, 127, 127, 127)
     else
         self.p.spr.tintColour = U.colour(255, 255, 255, 255)
+    end
+
+    if Key:key("w") then
+        self.c1.y = self.c1.y - 100 * dt
+    elseif Key:key("s") then
+        self.c1.y = self.c1.y + 100 * dt
+    end
+
+    if Key:key("a") then
+        self.c1.x = self.c1.x - 100 * dt
+    elseif Key:key("d") then
+        self.c1.x = self.c1.x + 100 * dt
+    end
+
+    if U.circle_to_circle_col(self.c1,  self.c2) then
+        self.c1.c = U.colour(0, 127, 127, 127)
+    else
+        self.c1.c = U.colour(255, 255, 255, 255)
     end
 end
 
@@ -34,6 +55,12 @@ function T:draw()
     love.graphics.clear(64,64,255)    
     -- love.graphics.print("Hello from test!", 200, 25)
     self.super.draw(self)
+
+    love.graphics.setColor(self.c1.c)
+    love.graphics.circle("line", self.c1.x, self.c1.y, self.c1.r)
+    love.graphics.setColor(self.c2.c)
+    love.graphics.circle("line", self.c2.x, self.c2.y, self.c2.r)
+    
 end
 
 return T
